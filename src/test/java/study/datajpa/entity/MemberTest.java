1bdab86b1645e3386918929e9d4b05cd16b73ccd
utopiandreams <berserkppp@naver.com>
2023-07-21 15:48:41 +0900

    Rollback false 삭제

--- src/test/java/study/datajpa/entity/MemberTest.java
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -2,19 +2,13 @@ package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
-@Rollback(value = false)
 class MemberTest {
     @PersistenceContext
     EntityManager em;
@@ -45,7 +39,7 @@ class MemberTest {
 
         em.flush(); em.clear();
 
-        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();;
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
         for (Member member : members) {
             System.out.println("member = " + member);
             System.out.println("member.getTeam() = " + member.getTeam());
